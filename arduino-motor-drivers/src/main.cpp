c65bb221344331fc2a641f5cc1a76732d0c450b4
Pau Alcolea Vila <paualcoleavila@Paus-MacBook-Air-475.local>
2026-02-12 10:38:16 -0500

    change testing code so that it only occurs once, repressing the button resets

--- arduino-motor-drivers/src/main.cpp
+++ arduino-motor-drivers/src/main.cpp
@@ -4,6 +4,7 @@ int is0601 = true;
 
 uint16_t startTime = 0;
 uint16_t elapsedTime = 0;
+boolean runTest = true;
 
 void setup()
 {
@@ -18,20 +19,29 @@ void setup()
 }
 
 void loop(){
-  startTime = micros();
-  Herkulex.moveOne(n, 100, 300, LED_BLUE, true);
-  elapsedTime = micros() - startTime;
-  Serial.print("time to send moveOne cmd: ");
-  Serial.print(elapsedTime);
-  Serial.println(" microseconds");
-  delay(1200);
-  startTime = micros();
-  Herkulex.getAngle(n, is0601);
-  elapsedTime = micros() - startTime;
-  Serial.print("time to read position: ");
-  Serial.print(elapsedTime);
-  Serial.println(" microseconds");
-  Herkulex.moveOne(n, 1000, 300, LED_BLUE, true);
-  delay(1200);
+  // this if statement makes sure that this only runs once at the beginning
+  if (runTest) {
+    startTime = micros();
+    Herkulex.moveOne(n, 100, 300, LED_BLUE, true);
+    elapsedTime = micros() - startTime;
 
+    Serial.print("time to send moveOne cmd: ");
+    Serial.print(elapsedTime);
+    Serial.println(" microseconds");
+
+    delay(1200);
+    
+    // reading the time needed for the angle of the motor to get back
+    startTime = micros();
+    Herkulex.getAngle(n, is0601);
+    elapsedTime = micros() - startTime;
+
+    Serial.print("time to read position: ");
+    Serial.print(elapsedTime);
+    Serial.println(" microseconds");
+
+    Herkulex.moveOne(n, 1000, 300, LED_BLUE, true);
+    delay(1200);
+  }
+  runTest = false;
 }
